d489214af0523535ee454575c9bbff4de6fd326e
xxxch <xxxch@DESKTOP-F8TU9FP>
2024-10-06 23:50:03 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,21 @@
+﻿double x = 3.7;
+
+Console.Write("Введи a = ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+// Вычисляем логарифм от абсолютного значения (cos(x) - 5)
+double logg = Math.Log(Math.Abs(Math.Cos(x) - 5));
+
+// Вычисляем экспоненту от cos(x)
+double expon = Math.Exp(Math.Cos(x));
+
+// Извлекаем корень из (a + x^2 * ln(|cos(x) - 5|))
+double sqrtT = Math.Sqrt(a + x * x * logg);
+
+
+// Делим на экспоненту
+double Resultat = sqrtT / expon;
+
+// Выводим результат
+Console.WriteLine("Ответ: " + Resultat);
+
